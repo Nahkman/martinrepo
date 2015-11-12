646a093ff4d36efed846a4670d0ca0e30fc9fcf3
Martin Kokser <martin.kokser@cgi.com>
2015-11-12 14:47:21 +0200

    first commit

---
+++ GitTest/src/com/gittest/martin/GitTest.java
@@ -0,0 +1,11 @@
+package com.gittest.martin;
+
+public class GitTest {
+	
+	public static void main(String[] args) {
+		
+		String hello = "Hello Git!";
+		int holdvalue = 13;
+		System.out.println(hello);
+	}
+}
